bb40ad1db027b425cc6ccfdc5002c93e86db968d
Sopita Punyakham <amint.dinger@gmail.com>
2017-06-25 04:12:47 +0700

    delete trash

--- app/src/main/java/com/example/cookzyme/cookzyme/Splash.java
+++ app/src/main/java/com/example/cookzyme/cookzyme/Splash.java
@@ -95,13 +95,6 @@ public class Splash extends AppCompatActivity {
             }
         });
 
-        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                Intent regIntent = new Intent(Splash.this, TextTranslate.class);
-                startActivity(regIntent);
-            }
-        });
-
         this.imageView = (ImageView)this.findViewById(R.id.imageView);
 
         Button photoButton = (Button) this.findViewById(R.id.camera);
@@ -172,22 +165,6 @@ public class Splash extends AppCompatActivity {
 
             //((EditText)findViewById(R.id.TTSeditText)).setText(imagePath);
         }
-    }
-
-    public static String encodeTobase64(Bitmap image)
-    {
-        Bitmap immagex=image;
-        ByteArrayOutputStream baos = new ByteArrayOutputStream();
-        immagex.compress(Bitmap.CompressFormat.PNG, 100, baos);
-        byte[] b = baos.toByteArray();
-        String imageEncoded = Base64.encodeToString(b,Base64.DEFAULT);
-        return imageEncoded;
-    }
-
-    public static Bitmap decodeBase64(String input)
-    {
-        byte[] decodedByte = Base64.decode(input, 0);
-        return BitmapFactory.decodeByteArray(decodedByte, 0, decodedByte.length);
     }
 
 }
